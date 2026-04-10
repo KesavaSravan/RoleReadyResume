6c49b4818aa1f3863fcfbda337c5c72b2e3a78a1
unknown <kesavasravan467@gmailcom>
2026-04-10 14:44:06 +0530

    Add latest uncommitted updates

---
+++ backend/test-pdf.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const pdfParse = require('pdf-parse');
+
+async function testPdf() {
+    // try to load some dummy pdf if exists, or how can I test it?
+    console.log("pdfParse version: ", require('pdf-parse/package.json').version);
+}
+testPdf();
